75271fe2d5478ccd601a9a75145d5cc8417f1e33
Alexander Baran <alexanderbaran@live.com>
2018-10-10 02:35:18 +0200

    .

--- misc/observables-final/users.service.ts
+++ misc/observables-final/users.service.ts
@@ -1,5 +1,11 @@
-import { Subject } from 'rxjs/Subject';
+// import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
 export class UsersService {
-  userActivated = new Subject();
+    // EventEmitter extends Subject
+    /* The Angular2 team stressed the fact though, that EventEmitter should not be used for anything
+    else then @Output()s in components and directives. Also EventEmitter gets cleaned up automatically
+    unlike custom Subjects that you need to unsubscribe to in the onDestroy lifecycle hook. */
+    // https://stackoverflow.com/questions/40238549/angular-2-event-emitters-vs-subject
+    userActivated = new Subject();
 }
